a7271dce16fda0262ba8fa816f3b24e7492e636f
shockfox <shockfox@209-4>
2024-10-03 12:31:01 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    Console.WriteLine("Добро пожаловать введите 4 значения");
+    double A = double.Parse(Console.ReadLine());
+    double B = double.Parse(Console.ReadLine());
+    double C = double.Parse(Console.ReadLine());
+    double D = double.Parse(Console.ReadLine());
+
+    bool log = (A % 7 == 0) || (B % 7 == 0) || (C % 7 == 0) || (D % 7 == 0);
+    bool gol = (A % 2 != 0) || (B % 2 != 0) || (C % 2 != 0) || (D % 2 != 0);
+    bool result = log && gol;
+    Console.WriteLine("Условие выполнено: " + result);
+
+}
+catch
+{
+    Console.WriteLine();
+}
